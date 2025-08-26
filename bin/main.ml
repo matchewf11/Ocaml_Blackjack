8534ea7bd4e8a995cdf909c400660d4b5dad22a2
Matthew Franchini <matchewf11@gmail.com>
2025-08-25 23:32:06 -0700

    some additions

--- bin/main.ml
+++ bin/main.ml
@@ -1,20 +1,21 @@
-(* Window Fields *)
-let game_name = "Blackjack"
+(* Absolute Fields *)
 let target_fps = 60
-let header_height = 30
 let window_width = 800
 let window_height = 600
+let init_balence = 500
 
-(* Header fields *)
-let cushion = 2
-let font_size = 20
-let balence_x = window_width / 4
+(* Relative fields *)
+let header_height = window_height / 20
+let font_size = (window_width + window_height) / 70
+let cushion = (window_width + window_height) / 700
+let balance_x = window_width / 4
 let bet_x = window_width / 2
 let fps_x = window_width / 4 * 3
-
-(* Game Fields *)
-let init_balence = 500
 let init_bet = init_balence / 5
+let deck_text_x = (window_width / 2) - (font_size * 2)
+let deck_text_y = window_height / 8
+let card_width = window_width / 8
+let card_height = window_height / 4
 
 type phase = Betting | Playing | Won | Lost
 type card = { rank : string; suit : string }
@@ -38,29 +39,27 @@ let update_game game_state = game_state
 let draw_game game_state =
   let open Raylib in
   begin_drawing ();
-  clear_background Color.raywhite;
 
-  (* Draw Header *)
   draw_rectangle 0 0 window_width header_height Color.brown;
   draw_text
     ("Phase: " ^ string_of_phase game_state.phase)
     cushion cushion font_size Color.black;
   draw_text
     ("Balance: " ^ string_of_int game_state.player.balance)
-    balence_x cushion font_size Color.black;
+    balance_x cushion font_size Color.black;
   draw_text
     ("Bet: " ^ string_of_int game_state.player.bet)
     bet_x cushion font_size Color.black;
   draw_fps fps_x cushion;
 
-  (* Draw Board *)
   draw_rectangle 0 header_height window_width
     (window_height - header_height)
     Color.green;
 
-  (* draw deck *)
-  (* draw player hand *)
-  (* draw dealer hand *)
+  draw_rectangle deck_text_x deck_text_y card_width card_height Color.red;
+  draw_text "Deck" deck_text_x deck_text_y font_size Color.black;
+
+  (* draw player and dealer hand *)
   end_drawing ()
 
 let rec game_loop game_state =
@@ -72,7 +71,7 @@ let rec game_loop game_state =
   ()
 
 let setup_game () =
-  Raylib.init_window window_width window_height game_name;
+  Raylib.init_window window_width window_height "BlackJack";
   Raylib.set_target_fps target_fps;
   {
     phase = Betting;
